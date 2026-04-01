910d6dc4e7b1d184c6aaefd59a5ed7317e432e40
meg44k <yugomegane@gmail.com>
2026-04-01 23:41:06 +0900

    Add: 大まかなAPIを作りました

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/labstack/echo/v5"
 
-	return;
+	"github.com/meg44k/KCTFestNav-Backend/internal/router"
+)
+
+func main() {
+	e := echo.New()
+
+	router.InitRoutes(e)
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
 }
