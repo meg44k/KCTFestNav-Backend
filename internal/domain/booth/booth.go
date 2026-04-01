9e907c444f8ec5f8d1c64c75ba8b6f0c456d3948
meg44k <yugomegane@gmail.com>
2026-04-01 18:43:13 +0900

    ブースにX,Y,Z座標を追加しました 将来的に3Dでブースの場所を扱うため、そのためにXYZ座標系を追加しました。 フロントエンド側で扱うことになるともしかしたら不要になるかもしれません。

--- internal/domain/booth/booth.go
+++ internal/domain/booth/booth.go
@@ -2,16 +2,21 @@
 
 package booth
 
-import "errors"
+import (
+	"errors"
+)
 
 // クラス展示、部活の出店などをあらわす型。
 // 混雑度は専用メソッドから読み取り、書き込みを行う必要があります。
 type Booth struct {
-	ID               string // ブースID
-	Name             string // ブース名
-	Organizer        string // ブースの主催者(ex. 1-1, 陸上部...)
-	Detail           string // ブースの説明
-	congestionStatus int    // 0: 空き 1: 少し混雑している 2: かなり混雑している
+	ID               string  // ブースID
+	Name             string  // ブース名
+	Organizer        string  // ブースの主催者(ex. 1-1, 陸上部...)
+	Detail           string  // ブースの説明
+	congestionStatus int     // 0: 空き 1: 少し混雑している 2: かなり混雑している]
+	X                float32 // X座標
+	Y                float32 // Y座標
+	Z                float32 // Z座標
 }
 
 // 新しいBoothのインスタンスを作成します
@@ -20,6 +25,10 @@ func NewBooth(
 	name string,
 	organizer string,
 	detail string,
+	X float32,
+	Y float32,
+	Z float32,
+
 ) *Booth {
 	return &Booth{
 		ID:               ID,
@@ -27,6 +36,9 @@ func NewBooth(
 		Organizer:        organizer,
 		Detail:           detail,
 		congestionStatus: 0,
+		X:                X,
+		Y:                Y,
+		Z:                Z,
 	}
 }
 
