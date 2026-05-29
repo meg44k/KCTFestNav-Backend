5c760e18110c344e2324213e514c265afc1e805f
meg44k <yugomegane@gmail.com>
2026-05-29 09:42:26 +0900

    Fix: NewBoothの戻り値変更に伴うテストの修正
    NewBooth関数がエラーを返すシグネチャに変更されたため、テストコードでの呼び出し箇所を修正しました。

--- internal/domain/booth_test.go
+++ internal/domain/booth_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestBoothCongestionStatus(t *testing.T) {
-	booth := NewBooth( "テストブース", "1-1", "テストブースです", 0, 0, 0)
+	booth, _ := NewBooth("テストブース", "1-1", "テストブースです", 0, 0, 0)
 	t.Run("正常値のセットチェック", func(t *testing.T) {
 		booth.SetCongestionStatus(1)
 		result := booth.congestionStatus
